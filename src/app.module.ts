52c7e5ad4212395b926a4a469673f7de48747860
Lautaro <lautaro.romczyk@gmail.com>
2024-06-18 16:13:55 -0300

    Create modules, controllers and services + connect DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { OperationsModule } from './operations/operations.module';
+import { RecordsModule } from './records/records.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Operation } from './operations/operation.entity';
+import { Record } from './records/record.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Operation, Record],
+      synchronize: true,
+    }),
+    UsersModule,
+    OperationsModule,
+    RecordsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
